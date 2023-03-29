d06b12ac875f99fb03f9e6fe41047ab300b7a6a7
toptalhook <toptal.cook@gmail.com>
2023-03-29 19:45:51 +0430

    create product model & migration and insert data with console(php artisan tinker)

---
+++ database/migrations/2023_03_29_145153_create_products_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->comment('user who created this product.');
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->float('price')->default(0);
+            $table->string('image')->nullable();
+
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Product;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    dd(Product::all());
     return view('welcome');
 });
