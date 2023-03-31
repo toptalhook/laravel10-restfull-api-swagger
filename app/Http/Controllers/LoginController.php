f09aac0f83952aada90cb74641bb7b979fb81f0b
toptalhook <toptal.cook@gmail.com>
2023-03-31 15:31:44 +0430

    Repository Pattern (Solid principle)

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,33 +2,26 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\User;
+use App\Http\Requests\LoginRequest;
+use App\Repositories\AuthRepository;
 use App\Traits\ResponseTrait;
-use Carbon\Carbon;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Hash;
+use Exception;
 
 class LoginController extends Controller
 {
     use ResponseTrait;
 
-    public function login(Request $request)
+    public function __construct(private AuthRepository $auth)
     {
-        // check the request for the valid user email
-        $user = User::query()->where('email', $request->email)->first();
-        if (!$user) {
-            return $this->responseSuccess([], 'User not found.');
-        }
-        // check the password
-        if (Hash::check($request->password, $user->password)) {
-            $createdToken = $user->createToken('authToken');
-            $data = [
-                'user' => $user,
-                'access_token' => $createdToken->accessToken,
-                'token_type' => 'Bearer',
-                'expires_at' => Carbon::parse($createdToken->token->expires_at)->toDateTimeString()
-            ];
-            return $this->responseSuccess($data, "Logged in successfully.");
+        $this->auth = $auth;
+    }
+
+    public function login(LoginRequest $request)
+    {
+        try {
+            return $this->auth->login($request->all());
+        }catch (Exception $exception){
+            return $this->responseError([], $exception->getMessage());
         }
     }
 }
---
+++ app/Http/Requests/LoginRequest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Requests;
+
+class LoginRequest extends ApiFormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'email' => 'required|email|max:100',
+            'password' => 'required|min:6'
+        ];
+    }
+
+}
